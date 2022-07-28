16a73e941fcf83462d9d50fbc53254610a889f77
aebaadcode <piotr.jarzebowski@gmail.com>
2022-07-28 18:22:45 -0400

    taser ammo odd number fix

--- Item_Ammo.cs
+++ Item_Ammo.cs
@@ -3,6 +3,6 @@ datablock ItemData(AE_TaserAmmoItem : AE_AmmoItem)
 	//shapeFile = "./dts/ammo/heavyrifle.dts";
 	uiName = "A: Taser";
 	AEAmmo = "Taser";
-	AEMax = 3;
-	AERefill = 1;
+	AEMax = 8;
+	AERefill = 2;
 };
